6e4830759eeb6fe7573dbc3ebdf23ee5c4ac9dd8
SudalaiMenon <sudalai.menon7@gmail.com>
2021-05-24 21:47:52 +0530

    initial commit

---
+++ Maptree.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Maptree {
+
+    public static void main(String[] args)
+    {
+        Map<String, Integer> map
+                = new TreeMap<>();
+
+        map.put("vishal", 10);
+        map.put("sachin", 30);
+        map.put("vaibhav", 20);
+
+        for (Map.Entry<String, Integer> e : map.entrySet())
+            System.out.println(e.getKey() + " "
+                    + e.getValue());
+    }
+}
---
+++ SetDemo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class SetDemo {
+    public static void main(String[] args)
+    {
+
+        Set<String> hash_Set = new HashSet<String>();
+
+        hash_Set.add("welcome");
+        hash_Set.add("Hi");
+        hash_Set.add("Home");
+        hash_Set.add("Hello");
+        hash_Set.add("Set");
+
+        System.out.println(hash_Set);
+    }
+}
